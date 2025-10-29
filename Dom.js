f3d64bf23d08df86e2d7c7b6800fe65dc2515577
Falouhiayoub <falouhiayoub9@gmail.com>
2025-10-29 21:35:13 +0100

    dom manipulation

---
+++ Dom.js
@@ -0,0 +1,74 @@
+// function changeColor() {
+//     const paragraph = document.getElementById("box")
+
+//     paragraph.style.color = 'red'
+    
+// }
+
+// function hideElement(){
+//     const paragraph = document.getElementById("para")
+//     if(paragraph.style.display === 'none') {
+//         paragraph.style.display = 'block'
+//     } else {
+//         paragraph.style.display = 'none'
+//     }
+// }
+
+// function changeText () {
+//     const paragraph = document.getElementById("para")
+//     paragraph.innerHTML = 'You Changed the text'
+// }
+
+// function changeDiv() {
+//     const div = document.getElementById('para')
+//     div.style.backgroundColor = 'red'
+// }
+// let countItem =2
+// function showList() {
+//     const ul = document.getElementById('list')
+//     countItem++ 
+//     const newItem = document.createElement('li')
+//     newItem.textContent = `Item ${countItem}`
+//     ul.appendChild(newItem)
+// }
+
+// function updateText() {
+//     const input = document.getElementById('myInput')
+//     const p = document.getElementById('displayPara')
+//     p.textContent = input.value
+// }
+
+// 1
+// function sayHello() {
+//     const input = document.getElementById("nameInput")
+//     const p = document.getElementById("greeting")
+
+//     p.textContent = 'hello' + " " + input.value + "!"
+// }
+
+// function checkAge() {
+//     const input = (document.getElementById('ageInput'))
+//     const result = document.getElementById('ageResult')
+//     if(input >= 18) {
+//         result.textContent = 'you are an adult'
+//     } else {
+//         result.textContent = 'you are a minor'
+//     }
+// }
+
+// function nameAndColor() {
+//     const nameInp = document.getElementById('nameInput')
+//     const colorInp = document.getElementById('colorInput')
+//     const p = document.getElementById('welcomeMsg')
+
+
+//     p.textContent = `hello ${nameInp.value} and your favorite color is ${colorInp.value}`
+// }
+
+// function reversedWord(str) {
+//     const reversedWord = document.getElementById('wordInput')
+//     const para = document.getElementById('myPara')
+//     for(let i = str.length - 1; i >= 0; i--){
+//         para.textContent = reversedWord.value
+//     }
+// }
