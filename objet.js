58a4de163c3b85de388e3fa68181f46e19c64b6e
Falouhiayoub <falouhiayoub9@gmail.com>
2025-10-29 10:01:37 +0100

    objects

---
+++ objet.js
@@ -0,0 +1,79 @@
+//1
+
+/*  function getValue(obj, key){
+
+   return obj[key]
+ 
+}
+console.log(getValue({ name: "abdo", age: 25 }, "age")); 
+console.log(getValue({ name: "abdo", age: 25 }, "city"));  */
+
+
+//2
+
+/* function setKey(obj, key, value){
+
+    return obj[key] = value;
+}
+let user = { name: "abdo" }
+
+setKey(user, "age" ,22)
+
+console.log(user )
+ */
+
+//3
+
+/* function removeKey(obj, key) {
+  delete obj[key];
+}
+const user = { name: "abdo", age: 21 };
+
+removeKey(user, "age");
+console.log(user);
+
+ */
+
+//4
+
+/*  const user = { name: "abdo", age: 21, city: "laayoune" };
+
+ let countKeys=Object.keys(user).length
+ 
+ console.log(countKeys) */
+
+ //5
+
+/*   const user = { name: "abdo", age: 21, city: "laayoune" };
+
+   let cond = user.hasOwnProperty("adresse")
+  
+    console.log(cond) */
+
+//6
+
+/* const a = { x: 1, y: 2 };
+const b = { y: 3, z: 4 };
+
+let mergeObjects = Object.assign(a,b)
+
+console.log(mergeObjects) */
+
+//7
+
+/*  const user = { name: "abdo", age: 21, city: "laayoune" };
+
+ let listkeys=Object.keys(user)
+
+ console.log(listkeys)  */
+
+ //8
+
+/*  const user = { name: "abdo", age: 21 };
+
+ let listvalues=Object.values(user)
+
+ console.log(listvalues)  */
+
+ //9
+
